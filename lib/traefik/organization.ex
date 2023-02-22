fa1de63bc2cf982a6cf77653dc2aa6581275897f
Octavio Lugo Camacho <octavio.lugo.makingdevs@kueski.com>
2023-02-21 20:34:25 -0600

    Refactor: Fix params in controller and organization

--- lib/traefik/organization.ex
+++ lib/traefik/organization.ex
@@ -2,13 +2,15 @@ defmodule Traefik.Organization do
   alias Traefik.Developer
   @devs_file Path.expand("./") |> Path.join("MOCK_DATA.csv")
 
-  def list_developers(_params \\ %{}) do
+  def list_developers(params \\ %{limit: 100, offset: 0}) do
     @devs_file
     |> File.read!()
     |> String.split("\n")
     |> Kernel.tl()
     |> Enum.map(&String.split(&1, ","))
     |> Enum.map(&transform_developer/1)
+    |> Enum.drop(params.offset)
+    |> Enum.take(params.limit)
     |> Enum.filter(&(&1 != nil))
   end
 
